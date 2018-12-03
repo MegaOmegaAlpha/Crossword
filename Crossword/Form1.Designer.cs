02dfcf9f2be7c984db7044f7687d310cd33a47b2
VladimirBai <bajjramov.vladimir@gmail.com>
2018-12-03 17:42:34 +0400

    Проектище начат

--- Crossword/Form1.Designer.cs
+++ Crossword/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPas = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(76, 48);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPas
+            // 
+            this.textBoxPas.Location = new System.Drawing.Point(80, 89);
+            this.textBoxPas.Name = "textBoxPas";
+            this.textBoxPas.Size = new System.Drawing.Size(96, 20);
+            this.textBoxPas.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(261, 199);
+            this.Controls.Add(this.textBoxPas);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxPas;
     }
 }
 
--- Crossword/Form1.cs
+++ Crossword/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace Crossword
         public Form1()
         {
             InitializeComponent();
+        }
+        SqlConnection sqlConnector;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connect = @"";
         }
     }
 }
